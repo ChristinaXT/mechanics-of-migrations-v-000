53efc8204eb6f2e71dd6cfa39d2d91b3415d0cf9
Christina Williams <christinaxtwilliams@gmail.com>
2019-02-07 17:07:15 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -2,5 +2,8 @@ class CreateArtists < ActiveRecord::Migration
   def change
   end
   
-  
+  ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 end
